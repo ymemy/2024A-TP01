f0c9d254fd4f6ef352d1ef618773ca69c4961f89
aminabenlala <aminabenlala@outlook.com>
2024-09-11 17:09:12 -0400

    print(2)

--- ex3.py
+++ ex3.py
@@ -1,5 +1,10 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
+import math
 
-speed = ... 
-angle = ...
+speed = float(input("Vitesse initiale de la boule:"))
+angle = float(input("Angle de lancement:"))
+angle_r = math.radians(angle)
+D = ((speed ** 2) * math.sin(2 * angle_r))/9.81
+D_max = round(D,2)
+print("La distance maximale en x", D_max ,"m")
